723c54c862a06393292484a4b64f8dd641a7b1fc
Patrick Dougall <pdougall1@gmail.com>
2018-03-28 11:37:25 -0400

    Cleans up world class

--- world.rb
+++ world.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 require_relative './life'
 
 class World
@@ -26,35 +27,12 @@ class World
   end
 
   def evaluate(life)
-    neighboring_coords = [
-      [life.x - 1, life.y - 1],
-      [life.x - 1, life.y],
-      [life.x - 1, life.y + 1],
-
-      [life.x,     life.y - 1],
-      [life.x,     life.y + 1],
-
-      [life.x + 1, life.y + 1],
-      [life.x + 1, life.y],
-      [life.x + 1, life.y - 1],
-    ]
-    
-    dead_neighbors = []
-    neighbors = neighboring_coords.reduce([]) { |neighbors, coords|
+    neighbors = neighboring_coords(life).reduce([]) { |neighbors, coords|
       if _life = @cells[get_location(*coords)]
         neighbors << _life
-      else
-        dead_neighbors << coords
       end
 
       neighbors
-    }
-
-    dead_neighbors.each { |dead_neighbor|
-      if should_come_alive(dead_neighbor)
-        life = Life.new(*coord)
-        @cells[get_location(life.x, life.y)] = life
-      end
     }
 
     if neighbors.count < 2 || neighbors.count > 3
@@ -71,4 +49,63 @@ class World
   def get_location(x, y)
     "#{x}:#{y}"
   end
+  
+  def neighboring_coords(life)
+    [
+      [life.x - 1, life.y - 1],
+      [life.x - 1, life.y],
+      [life.x - 1, life.y + 1],
+
+      [life.x,     life.y - 1],
+      [life.x,     life.y + 1],
+
+      [life.x + 1, life.y + 1],
+      [life.x + 1, life.y],
+      [life.x + 1, life.y - 1],
+    ]
+  end
 end
+
+# seed = [[0, 0]]
+# world = World.new(seed)
+# puts world.show_lives
+
+# class AnyClass
+
+#   def initialize
+#     return "THING I WANT"
+#   end
+
+#   def my_method
+#     return self
+#   end
+# end
+
+# puts AnyClass.new.class
+# puts AnyClass.new.my_method.class
+
+
+
+
+# class MyHash
+
+#   def initialize
+#     @my_hash = {}
+#   end
+
+#   def [](key)
+#     @my_hash[key]
+#   end
+
+#   def []=(key, value)
+#     @my_hash[key] = value
+#   end
+
+#   def get_thing
+#     @thing
+#   end
+
+#   def set_thing=(thing)
+#     @thing = thing
+#   end
+# end
